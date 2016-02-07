06ceb19283f8dbd121c643c6dae724947dccb8a7
fin-alice <enderalice@yahoo.com>
2016-02-08 08:59:39 +0900

    コマンドの処理をメソッドに分割

--- src/main/java/alice/entlimit/CommandEntLimit.java
+++ src/main/java/alice/entlimit/CommandEntLimit.java
@@ -19,6 +19,151 @@ import net.minecraftforge.common.DimensionManager;
 
 public final class CommandEntLimit implements ICommand
 {
+	private void list(ICommandSender sender)
+	{
+		sender.addChatMessage(new ChatComponentTranslation("command.entlimit.listhead"));
+
+		Map<Integer, Integer> m = EntityLimitManager.getLimitInWorlds();
+
+		m.forEach((d, l) ->
+		{
+			if(l.equals(EntityLimitManager.UNLIMITED_CONST))
+			{
+				sender.addChatMessage(new ChatComponentTranslation("command.entlimit.diminfodef", d));
+			}
+			else
+			{
+				sender.addChatMessage(new ChatComponentTranslation("command.entlimit.diminfo", d, l));
+			}
+		});
+
+		sender.addChatMessage(new ChatComponentTranslation("command.entlimit.listfoot"));
+	}
+
+	private void set(ICommandSender sender, String limit)
+	{
+		if(!(sender instanceof EntityPlayer))
+		{
+			// プレイヤーによって実行されていない
+			throw new CommandException("command.entlimit.nodimid");
+		}
+
+		EntityPlayer player = (EntityPlayer)sender;
+		World world = player.worldObj;
+		WorldProvider provider = world.provider;
+
+		this.set(sender, Integer.toString(provider.dimensionId), limit);
+	}
+
+	private void set(ICommandSender sender, String dimension, String limit)
+	{
+		if(sender instanceof EntityPlayerMP)
+		{
+			if(!sender.canCommandSenderUseCommand(3, "entlimit"))
+			{
+				throw new CommandException("command.entlimit.op", sender.getCommandSenderName());
+			}
+		}
+
+		int dimensionId;
+		try
+		{
+			dimensionId = Integer.parseInt(dimension);
+		}
+		catch(NumberFormatException e)
+		{
+			throw new CommandException("command.entlimit.baddimid", dimension);
+		}
+		if(!DimensionManager.isDimensionRegistered(dimensionId))
+		{
+			throw new CommandException("command.entlimit.baddimid", dimension);
+		}
+
+		int limitNum;
+		try
+		{
+			limitNum = Integer.parseInt(limit);
+			if(limitNum < 1)
+			{
+				throw new NumberFormatException();
+			}
+		}
+		catch(NumberFormatException e)
+		{
+			throw new CommandException("command.entlimit.badnum", limit);
+		}
+
+		EntityLimitManager.setLimitForWorld(dimensionId, limitNum);
+		sender.addChatMessage(new ChatComponentTranslation("command.entlimit.set", dimension, limit));
+	}
+
+	private void clear(ICommandSender sender)
+	{
+		if(!(sender instanceof EntityPlayer))
+		{
+			// プレイヤーによって実行されていない
+			throw new CommandException("command.entlimit.nodimid");
+		}
+
+		EntityPlayer player = (EntityPlayer)sender;
+		World world = player.worldObj;
+		WorldProvider provider = world.provider;
+
+		this.clear(sender, Integer.toString(provider.dimensionId));
+	}
+
+	private void clear(ICommandSender sender, String dimension)
+	{
+		if(sender instanceof EntityPlayerMP)
+		{
+			if(!sender.canCommandSenderUseCommand(3, "entlimit"))
+			{
+				throw new CommandException("command.entlimit.op", sender.getCommandSenderName());
+			}
+		}
+
+		int dimensionId;
+		try
+		{
+			dimensionId = Integer.parseInt(dimension);
+		}
+		catch(NumberFormatException e)
+		{
+			throw new CommandException("command.entlimit.baddimid", dimension);
+		}
+		if(!DimensionManager.isDimensionRegistered(dimensionId))
+		{
+			throw new CommandException("command.entlimit.baddimid", dimension);
+		}
+
+		EntityLimitManager.removeLimitForWorld(dimensionId);
+		sender.addChatMessage(new ChatComponentTranslation("command.entlimit.clear", dimension));
+	}
+
+	private void vanish(ICommandSender sender)
+	{
+		if(sender instanceof EntityPlayerMP)
+		{
+			if(!sender.canCommandSenderUseCommand(3, "entlimit"))
+			{
+				throw new CommandException("command.entlimit.op", sender.getCommandSenderName());
+			}
+		}
+
+		MinecraftServer server = MinecraftServer.getServer();
+		if(server == null)
+		{
+			return;
+		}
+
+		Stream<WorldServer> streamWorld = Stream.of(server.worldServers);
+		// 要素数1の配列を使うことで参照渡しっぽいやり方ができる
+		final int[] killCount = new int[1];
+		killCount[0] = 0;
+		streamWorld.forEach(w -> EntityLimiter.vanishOvers(w, killCount));
+		sender.addChatMessage(new ChatComponentTranslation("command.entlimit.vanished", Integer.toString(killCount[0])));
+	}
+
 	@Override
 	public int compareTo(Object o)
 	{
@@ -53,154 +198,41 @@ public final class CommandEntLimit implements ICommand
 		}
 
 		String action = argv[0].toUpperCase(Locale.ENGLISH);
-		int dimension;
-		int limit;
 		switch(action.charAt(0))
 		{
 		case 'L': // list
-			sender.addChatMessage(new ChatComponentTranslation("command.entlimit.listhead"));
-			Map<Integer, Integer> m = EntityLimitManager.getLimitInWorlds();
-			m.forEach((d, l) -> {
-				if(l.intValue() == -1)
-				{
-					sender.addChatMessage(new ChatComponentTranslation("command.entlimit.diminfodef", d));
-				}
-				else
-				{
-					sender.addChatMessage(new ChatComponentTranslation("command.entlimit.diminfo", d, l));
-				}
-			});
-			sender.addChatMessage(new ChatComponentTranslation("command.entlimit.listfoot"));
+			this.list(sender);
 			break;
 		case 'S': // set
-			if(sender instanceof EntityPlayerMP)
-			{
-				if(!sender.canCommandSenderUseCommand(3, "entlimit"))
-				{
-					throw new CommandException("command.entlimit.op", sender.getCommandSenderName());
-				}
-			}
 			if(argv.length >= 3)
 			{
 				// ディメンションIDが指定されている
-				try
-				{
-					dimension = Integer.parseInt(argv[1]);
-				}
-				catch(NumberFormatException e)
-				{
-					throw new CommandException("command.entlimit.baddimid", argv[1]);
-				}
-				if(!DimensionManager.isDimensionRegistered(dimension))
-				{
-					throw new CommandException("command.entlimit.baddimid", argv[1]);
-				}
-
-				try
-				{
-					limit = Integer.parseInt(argv[2]);
-					if(limit < 1)
-					{
-						throw new NumberFormatException();
-					}
-				}
-				catch(NumberFormatException e)
-				{
-					throw new CommandException("command.entlimit.badnum", argv[2]);
-				}
+				this.set(sender, argv[1], argv[2]);
 			}
 			else if(argv.length == 2)
 			{
 				// ディメンションIDが指定されていない
-				if(!(sender instanceof EntityPlayer))
-				{
-					// プレイヤーによって実行されていない
-					throw new CommandException("command.entlimit.nodimid");
-				}
-				EntityPlayer player = (EntityPlayer)sender;
-				World world = player.worldObj;
-				WorldProvider provider = world.provider;
-				dimension = provider.dimensionId;
-
-				try
-				{
-					limit = Integer.parseInt(argv[1]);
-					if(limit < 1)
-					{
-						throw new NumberFormatException();
-					}
-				}
-				catch(NumberFormatException e)
-				{
-					throw new CommandException("command.entlimit.badnum", argv[1]);
-				}
+				this.set(sender, argv[1]);
 			}
 			else
 			{
 				throw new CommandException("command.endlimit.badcmd");
 			}
-			EntityLimitManager.setLimitForWorld(dimension, limit);
-			sender.addChatMessage(new ChatComponentTranslation("command.entlimit.set", new Integer(dimension), new Integer(limit)));
 			break;
 		case 'C': // clear
-			if(sender instanceof EntityPlayerMP)
-			{
-				if(!sender.canCommandSenderUseCommand(3, "entlimit"))
-				{
-					throw new CommandException("command.entlimit.op", sender.getCommandSenderName());
-				}
-			}
 			if(argv.length >= 2)
 			{
 				// ディメンションIDが指定されている
-				try
-				{
-					dimension = Integer.parseInt(argv[1]);
-				}
-				catch(NumberFormatException e)
-				{
-					throw new CommandException("command.entlimit.baddimid", argv[1]);
-				}
-				if(!DimensionManager.isDimensionRegistered(dimension))
-				{
-					throw new CommandException("command.entlimit.baddimid", argv[1]);
-				}
+				this.clear(sender, argv[1]);
 			}
 			else
 			{
 				// ディメンションIDが指定されていない
-				if(!(sender instanceof EntityPlayer))
-				{
-					// プレイヤーによって実行されていない
-					throw new CommandException("command.entlimit.nodimid");
-				}
-				EntityPlayer player = (EntityPlayer)sender;
-				World world = player.worldObj;
-				WorldProvider provider = world.provider;
-				dimension = provider.dimensionId;
+				this.clear(sender);
 			}
-			EntityLimitManager.removeLimitForWorld(dimension);
-			sender.addChatMessage(new ChatComponentTranslation("command.entlimit.clear", new Integer(dimension)));
 			break;
 		case 'V': // vanish
-			if(sender instanceof EntityPlayerMP)
-			{
-				if(!sender.canCommandSenderUseCommand(3, "entlimit"))
-				{
-					throw new CommandException("command.entlimit.op", sender.getCommandSenderName());
-				}
-			}
-			MinecraftServer server = MinecraftServer.getServer();
-			if(server == null)
-			{
-				return;
-			}
-			Stream<WorldServer> streamWorld = Stream.of(server.worldServers);
-			// 要素数1の配列を使うことで参照渡しっぽいやり方ができる
-			final int[] killCount = new int[1];
-			killCount[0] = 0;
-			streamWorld.forEach(w -> EntityLimiter.vanishOvers(w, killCount));
-			sender.addChatMessage(new ChatComponentTranslation("command.entlimit.vanished", killCount[0]));
+			this.vanish(sender);
 			break;
 		default:
 			throw new CommandException("command.entlimit.unknownsubcmd", argv[0]);
